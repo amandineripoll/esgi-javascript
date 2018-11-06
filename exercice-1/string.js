179cb360f01b4f974a730050998277cfcbb7888f
Amandine <amandine-ripoll@outlook.fr>
2018-11-06 17:11:39 +0100

    Exo fait jusqu'a leet

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -13,10 +13,39 @@ function capitalize(string) {
 }
 
 function camelCase(string){
+  if(typeof string === 'string'){
+    return (capitalize(string)).replace(/ /g, "");
+  }
+  else {
+    return null;
+  }
+}
+
+function snake_case(string){
+  return (string.replace(/ /g, "_")).toLowerCase();
+}
+
+function leet(string){
+var tabCryptage = { "A" : "4", "E" : "3", "I": "1", "O": "0", "U" : "(_)", "Y" : "7"};
+var data = "";
+for (var i = 0; i < string.length; i++){
+    if(typeof tabCryptage[string[i].toUpperCase()] === 'undefined') {
+        data += string[i];
+    }
+    else {
+        data += tabCryptage[string[i].toUpperCase()];
+    }
+}
+return data;
+}
+
+function prop_access(){
 
 }
 
 
-
-console.log(ucfirst('coucou'));
-console.log(capitalize('bonjour amandine'));
+// console.log(ucfirst('coucou'));
+// console.log(capitalize('bonjour amandine'));
+// console.log(camelCase('bonjour amandine la boss'));
+// console.log(snake_case('bonjour amandine la boss'));
+// console.log(leet('amandine'));
